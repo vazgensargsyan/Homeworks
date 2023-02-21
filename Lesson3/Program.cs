8f6513e05a59c53d3550d277f119e89fe402ad12
Vazgen Sargsyan <vazgensargsyan2@gmail.com>
2023-02-21 20:18:00 +0400

    calculator

---
+++ Lesson3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+
+namespace Lesson3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a math operation (not valid)");
+            string x = Console.ReadLine();
+            DataTable dt = new DataTable();
+            Console.WriteLine(dt.Compute(x, ""));
+        }
+    }
+}
