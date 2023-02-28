5ed8a9e5bb4b9bc45469318cbffb972736fde5c8
Vazgen Sargsyan <vazgensargsyan2@gmail.com>
2023-02-28 17:14:23 +0400

    try catch

---
+++ Lesson5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lesson5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write anyone text");
+            dynamic x = Console.ReadLine();
+            int y = 0;
+            Sum sum = new Sum();
+            try
+            {
+                sum.Calculation(x, ref y);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Message: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+            Console.WriteLine(y);
+        }
+    }
+}
---
+++ Lesson5/Sum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5
+{
+    internal class Sum
+    {
+        public int Calculation(dynamic x, ref int y)
+        {
+            dynamic result = 152;
+            result += x;
+            y++;
+            return result;
+        }
+    }
+}
