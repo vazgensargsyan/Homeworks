9221ac9f33be4ea2fc4f5fce2b5a7d269ebef73e
Vazgen Sargsyan <vazgensargsyan2@gmail.com>
2023-03-02 17:47:48 +0400

    lesson7

---
+++ Lesson7/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Lesson7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int riceCount = 40;             
+            int fishCount = 10;    
+            int cucumberCount = 50;
+            int meetCount = 20;    
+            int sausageCount = 30; 
+            int breadCount = 100;
+            Console.WriteLine("1. If you want make SUSHI press 1");
+            Console.WriteLine("2. If you want make HOTDOG press 2");
+            Console.WriteLine("3. If you want make BURGER press 3");
+            Console.WriteLine("3. If you want make one SUSHI and one HOTDOG and one BURGER press 4");
+            Console.Write("Write number and press enter - ");
+            try
+            {
+                byte whatMake = Byte.Parse(Console.ReadLine());
+                Restaurant restaurant = new Restaurant();
+                switch (whatMake)
+                {
+                    case 1:
+                        Console.Write("Enter sushi`s count - ");
+                        int sushiCount = Int32.Parse(Console.ReadLine());
+                        restaurant.MakeSushi(in sushiCount, ref riceCount, ref fishCount, ref cucumberCount);
+                        break;
+                    case 2:
+                        Console.Write("Enter hotdog`s count - ");
+                        int hotdogCount = Int32.Parse(Console.ReadLine());
+                        restaurant.MakeHotDog(in hotdogCount, ref sausageCount, ref breadCount);
+                        break;
+                    case 3:
+                        Console.Write("Enter burger`s count - ");
+                        int burgerCount = Int32.Parse(Console.ReadLine());
+                        restaurant.MakeBurger(in burgerCount, ref meetCount, ref breadCount);
+                        break;
+                    case 4:
+                        Restaurant restaurantAll = new Restaurant(ref riceCount, ref fishCount, ref cucumberCount, ref sausageCount, ref meetCount, ref breadCount);
+                        break;
+                    default:
+                        throw new Exception(nameof(Messages.EnterTheCorrectNumber));
+                        break;
+                }                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Rice - " + riceCount + " Fish - " + fishCount + " Cucumber - " + cucumberCount + " Meet - " + meetCount + " Sausage - " + sausageCount + " Bread - " + breadCount);
+            }
+        }
+    }
+}
---
+++ Lesson7/Restaurant.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7
+{
+    enum Messages
+    {
+        BonAppetit,
+        AnInternalServerError,
+        YouDoNotHaveEnoughFoodToMakeSushi,
+        YouDoNotHaveEnoughFoodToMakeHotDog,
+        YouDoNotHaveEnoughFoodToMakeHotBurger,
+        EnterTheCorrectNumber
+    }
+    internal class Restaurant
+    {
+        const int ONESUSHIRICECOUNT = 4;
+        const int ONESUSHIFISHCOUNT = 1;
+        const int ONESUSHICUCUMBERCOUNT = 3;
+        const int ONEHOTDOGSAUSAGECOUNT = 2;
+        const int ONEHOTDOGBREADCOUNT = 1;
+        const int ONEBURGERBREADCOUNT = 1;
+        const int ONEBURGERMEETCOUNT = 1;
+        public Restaurant()
+        {
+            
+        }
+        public Restaurant(ref int riceCount, ref int fishCount, ref int cucumberCount, ref int sausageCount, ref int meetCount, ref int breadCount)
+        {
+            int count = 1;
+            MakeSushi(in count, ref riceCount, ref fishCount, ref cucumberCount);
+            MakeHotDog(in count, ref sausageCount, ref breadCount);
+            MakeBurger(in count, ref meetCount, ref breadCount);
+        }
+        public void MakeSushi(in int sushiCount, ref int riceCount, ref int fishCount, ref int cucumberCount)
+        {
+            if(riceCount - sushiCount * ONESUSHIRICECOUNT >= 0 && fishCount - sushiCount * ONESUSHIFISHCOUNT >= 0 && cucumberCount - sushiCount * ONESUSHICUCUMBERCOUNT >= 0)
+            {
+                riceCount -= sushiCount * ONESUSHIRICECOUNT;
+                fishCount -= sushiCount * ONESUSHIFISHCOUNT;
+                cucumberCount -= sushiCount * ONESUSHICUCUMBERCOUNT;
+            }
+            else
+            {
+                throw new Exception(nameof(Messages.YouDoNotHaveEnoughFoodToMakeSushi));
+            }
+            Console.WriteLine(Messages.BonAppetit);
+        }
+        public void MakeHotDog(in int hotDogCount, ref int sausageCount, ref int breadCount)
+        {
+            if(sausageCount - hotDogCount * ONEHOTDOGSAUSAGECOUNT >= 0 && breadCount - hotDogCount * ONEHOTDOGBREADCOUNT >= 0)
+            {
+                sausageCount -= hotDogCount * ONEHOTDOGSAUSAGECOUNT;
+                breadCount -= hotDogCount * ONEHOTDOGBREADCOUNT;
+            }
+            else
+            {
+                throw new Exception(nameof(Messages.YouDoNotHaveEnoughFoodToMakeHotDog));
+            }
+            Console.WriteLine(Messages.BonAppetit);
+        }
+        public void MakeBurger(in int burgerCount, ref int meetCount, ref int breadCount)
+        {
+            if(meetCount - burgerCount * ONEBURGERMEETCOUNT >= 0 && breadCount - burgerCount * ONEHOTDOGBREADCOUNT >= 0)
+            {
+                meetCount -= burgerCount * ONEBURGERMEETCOUNT;
+                breadCount -= burgerCount * ONEBURGERBREADCOUNT;
+            }
+            else
+            {
+                throw new Exception(nameof(Messages.YouDoNotHaveEnoughFoodToMakeHotBurger));
+            }
+            Console.WriteLine(Messages.BonAppetit);
+        }
+    }
+}
