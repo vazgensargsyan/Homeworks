157ac059e59941519d588a7359b306ebcf9aa730
Vazgen Sargsyan <vazgensargsyan2@gmail.com>
2023-03-20 14:19:00 +0400

    Lesson11

---
+++ Lesson11/Program.cs
@@ -0,0 +1,17 @@
+﻿using OOP_6_2;
+
+namespace Lesson11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If your office have IT privilege press 1 else 0!!!");
+            byte isPrivilege = Byte.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your DIRTY salary!!!");
+            decimal cSalary = decimal.Parse(Console.ReadLine());
+            CalcSalary calcSalary = new CalcSalary(cSalary, isPrivilege);
+            Console.WriteLine(calcSalary.ReturnInfo());
+        }
+    }
+}
